9bdbc8879ad49945bf733da46329f45ba118854a
big easy <vandecarl@gmail.com>
2023-11-07 16:30:17 +0100

    Server side response codes

--- getCatFact.php
+++ getCatFact.php
@@ -1,11 +1,16 @@
-<?php 
+<?php
 function getCatFact()
 {
-    $parameterValue = $_GET['param']; 
+    $parameterValue = $_GET['param'];
 
     if ($parameterValue !== '42') {
-        return 'Wrong input bitch!';
+        // Set a 403 Forbidden response code
+        http_response_code(403);
+        $data = 'Wrong input, access denied.';
+        echo $data;
+        return false;
     }
+
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, 'https://catfact.ninja/fact');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -13,18 +18,24 @@ function getCatFact()
     $response = curl_exec($ch);
 
     if (curl_errno($ch)) {
+        // Set a 404 Not Found response code for cURL errors
+        http_response_code(404);
         echo 'cURL error: ' . curl_error($ch);
-    }
-
-    curl_close($ch);
-
-    $data = json_decode($response);
-
-    if ($data && isset($data->fact)) {
-        echo $data->fact;
     } else {
-        echo 'No cat fact yet';
+        // Set a 200 OK response code for a successful response
+        http_response_code(200);
+
+        curl_close($ch);
+
+        $data = json_decode($response);
+
+        if ($data && isset($data->fact)) {
+            echo $data->fact;
+        } else {
+            echo 'No cat fact yet';
+        }
     }
 }
+
 getCatFact();
 ?>
